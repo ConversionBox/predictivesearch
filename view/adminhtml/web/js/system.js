47aa4310407fd9fba2c58646c4a377dddc964f4b
graciyas <graciyarachel@thecommerceshop.com>
2025-03-19 17:43:29 +0530

    remove the alert box

--- view/adminhtml/web/js/system.js
+++ view/adminhtml/web/js/system.js
@@ -54,44 +54,26 @@ require(
             let indexvalue =  $('#typesense_general_credentials_index_prefix').val();
             if (uniqueId == "" || indexvalue =="") {
                 alert({
-                    content: 'Enter the required field Unique ID or Index Name'
+                    content: 'Enter the required field Unique ID '
                 })
                 return false;
             }
-            alert({
-               content: 'Do you want to continue.',
-               buttons: [{
-                    text: $.mage.__('OK'),
-                    class: 'action-primary action-accept',
-                    click: function () {
                         $.ajax({
                             showLoader: true, 
                             url: window.baseDomian+'typesense/index/connection',
-                            data: {
-                                'uniqueId': uniqueId,
-                                'indexvalue':indexvalue
-                            },
                             type: "GET", 
                             dataType: 'json'
                         }).done(function (data) { 
                             $('#response_message').delay(1000).fadeIn();
                             if (data.success) {
-                                $('#response_message').html('Successfully connected to Conversionbox Merchant account');
-                                $('#response_message').css('color', '#097009');
-                                $('#response_message').css('background', '#f3f310');
-                                $('#response_message').delay(1000).fadeOut();
+                                $('#connection_index').text('Connected Successful!');
                             } else {
                                 $('#response_message').html('Something Went Wrong try again');
-                                $('#response_message').css('color', '#097009');
-                                $('#response_message').css('background', '#f3f310');
                                 $('#response_message').delay(1000).fadeOut();
+                                $('#connection_index').text('Connection Failed! Try Again');
                             }
 
                         });
-                        this.closeModal(true);
-                    }
-                }]
-            })
         });
         $('#reindex_data').click(function(e) {
             e.preventDefault();
